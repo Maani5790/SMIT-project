f1699a52683df255a5d486147a9c9ad3cce505b0
Abdul Rehman <arboacha@gmail.com>
2022-11-12 14:06:56 +0500

    SMIT PROJECT

---
+++ Config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYG_Xb8yhhAhzYyz_A4tS4DiiMqVviZMM",
+    authDomain: "smit-attendace-system.firebaseapp.com",
+    projectId: "smit-attendace-system",
+    storageBucket: "smit-attendace-system.appspot.com",
+    messagingSenderId: "11391237274",
+    appId: "1:11391237274:web:0593563e4c998ef9f8f323",
+    measurementId: "G-HV49VWE21V"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
